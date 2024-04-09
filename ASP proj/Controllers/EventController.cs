5b30aa2127c9953d00932e562a7912353face911
Arthur <arthurdragon39@gmail.com>
2024-04-08 23:41:53 -0500

    commit

---
+++ ASP proj/Controllers/EventController.cs
@@ -0,0 +1,37 @@
+﻿using ASP_proj.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP_proj.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly SiteContext _siteContext;
+        public EventController(SiteContext siteContext)
+        {
+            _siteContext = siteContext;
+        }
+        [Route("/event/index")]
+        public IActionResult Index()
+        {
+            return View(_siteContext.Events.Include(x => x.Name).ToList());
+        }
+        [HttpGet("/event/create")]
+        public IActionResult Create()
+        {
+            ViewData["Title"] = "Create Event";
+            return View(new Event());
+        }
+        //[HttpPost("/event/create")]
+        //public IActionResult Create()
+        //{
+
+        //}
+        [HttpGet("/event/edit/{id}")]
+        public async Task<IActionResult> Edit(int id)
+        {
+            ViewData["Title"] = "Edit Event";
+            return View(await _siteContext.Events.FirstAsync(x => x.Id == id));
+        }
+    }
+}
