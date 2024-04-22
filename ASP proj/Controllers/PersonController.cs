fa179ebc8d5e525572ab1b125e22c9aa96637a60
Arthur <arthurdragon39@gmail.com>
2024-04-21 21:36:08 -0500

    commit

---
+++ ASP proj/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using ASP_proj.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_proj.Controllers
+{
+    public class PersonController : Controller
+    {
+        private readonly SiteContext _siteContext;
+        public PersonController(SiteContext siteContext)
+        {
+            _siteContext = siteContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
